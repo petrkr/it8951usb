31ebe74968a519586fdf761e406e1bd8e8a8ba75
Martijn Braam <martijn@brixit.nl>
2020-04-26 15:23:33 +0200

    Removed unnecessary headers

--- main.c
+++ main.c
@@ -1,13 +1,10 @@
 #include <unistd.h>
-#include <signal.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <sys/ioctl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <scsi/scsi.h>
 #include <scsi/sg.h>
 #include <byteswap.h>
